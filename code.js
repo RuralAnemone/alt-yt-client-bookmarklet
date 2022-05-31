5a7d229dd7cbc3ffb4395bce71c652f278b15066
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-05-31 17:19:17 -0500

    oops + no ;

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-var a = "il.ax"; /**replace this with your desired piped/invidious client**/  
+var a = "il.ax";
 
-if (window.location.href.match(/https?\:\/\/youtube\.com\/watch/)) {
-    window.open("https://" + a + "/watch" + window.location.search, "_blank");
+if (window.location.href.match(/https?\:\/\/w{3}?\.?youtube\.com\/watch/)) {
+    window.open("https://" + a + "/watch" + window.location.search, "_blank")
 } else {
-    window.open("https://" + a + "/watch" + prompt("you don't seem to be on a youtube video...\n\ncopy & paste the video url here:").match(/https?\:\/\/www\.youtube\.com\/watch(\?v=[0-9A-Za-z_\-]+)/)[1], "_blank");
+    window.open("https://" + a + "/watch" + prompt("you don't seem to be on a youtube video...\n\ncopy & paste the video url here:").match(/https?\:\/\/www\.youtube\.com\/watch(\?v=[0-9A-Za-z_\-]+)/)[1], "_blank")
 }
