3d58dfe8b56f0c0034f999d860135db2604c987f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-05-31 18:02:41 -0500

    buh what was I thinking

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var a = "il.ax";
 
-if (window.location.href.match(/https?\:\/\/w{3}?\.?youtube\.com\/watch/)) {
+if (window.location.href.match(/https?\:\/\/www\.youtube\.com\/watch/)) {
     window.open("https://" + a + "/watch" + window.location.search, "_blank")
 } else {
     window.open("https://" + a + "/watch" + prompt("you don't seem to be on a youtube video...\n\ncopy & paste the video url here:").match(/https?\:\/\/www\.youtube\.com\/watch(\?v=[0-9A-Za-z_\-]+)/)[1], "_blank")
